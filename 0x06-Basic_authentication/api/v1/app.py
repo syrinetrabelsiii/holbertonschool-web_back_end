15af9c69c63c286de2759caa89403bea94181234
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-02-07 22:25:11 +0100

    Add a new error handler

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -21,10 +21,7 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+
     
 @app.errorhandler(401)
 def Unauthorized(error) -> str:
