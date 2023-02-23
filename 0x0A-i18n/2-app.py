b6f60663f5a5690c6a0fb71d9663a6c831d80b86
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-02-23 23:12:54 +0100

    get locale from request

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -22,6 +22,7 @@ def get_locale():
     determine the best match with our supported languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+    app.config.from_object(Config)
 
 @app.route("/", methods=['GET'])
 def helloWorld():
