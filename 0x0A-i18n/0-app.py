c5d1e92d94655741708209ecf2e69192e3b2bad1
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-02-23 22:34:46 +0100

    basic flask aoo

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Welcome to Holberton
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    language config
+    """
+    LANGUAGES = ['en', 'es']
+
+
+@app.route("/", methods=['GET'])
+def helloWorld():
+    """
+    Hello world
+    """
+    return render_template('0-index.html')
