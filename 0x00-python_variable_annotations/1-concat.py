c27d6137e7a08d784c7d153dfa84e825a2666255
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-03-06 14:30:48 +0100

    basic annotations concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+concat that takes a string str1 and a string str2
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    returns str1 + str2
+    """
+    return "{}{}".format(str1, str2)
