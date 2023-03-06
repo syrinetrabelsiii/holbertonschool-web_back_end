35629731905de63307fcac2c226f3e790d6e3a29
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-03-06 14:28:16 +0100

    add func

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+add function a+ b
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    return the sum of two numbers as a float (a, b)
+    """
+    return a + b
