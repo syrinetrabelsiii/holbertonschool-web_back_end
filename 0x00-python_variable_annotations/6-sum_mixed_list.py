072d50c4a21e1a73d52033ec49df24cc96400705
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-03-06 18:55:47 +0100

    sum mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
 """
-type-annotated function mxd_list
-"""
+
 from typing import List, Union
 
 def sum_mixed_list (mxd_lst: List(Union[int, float])) -> float:
-    """ 
-    returns their sum as a float
-    """ 
+    """_summary_
+
+    Args:
+        mxd_lst (List): _description_
+
+    Returns:
+        float: _description_
+    """
     return sum(mxd_lst)
