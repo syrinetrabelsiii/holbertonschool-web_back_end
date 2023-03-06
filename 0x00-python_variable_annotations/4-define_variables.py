ef7ca0f147fee167f8cfd6218fa2fa645c7db8dd
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-03-06 17:47:08 +0100

    define var

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """
-Define and annotate the following variables with the specified values
+define and annotate variables with a specified values.
 """
-a : int = 1
-pi : float = 3.14
-i_understand_annotations : boolean = True
-school : str = "Holberton"
 
-
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton".
