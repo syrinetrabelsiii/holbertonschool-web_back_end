6b14456dae2f40d2a633322cf0a012279da205a8
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-03-06 18:48:43 +0100

    sum mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+     type-annotated function mxd_list
+"""
+from typing import List
+
+def sum_mixed_list (mxd_lst: List[int]) -> float:
+    """ returns their sum as a float
+    """ 
+    return sum(mxd_lst)
