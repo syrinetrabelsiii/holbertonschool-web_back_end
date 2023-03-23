fbf7b6da74b87d8169f90e8b7fe526a28d174226
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-03-23 21:17:54 +0100

    make clasroomm

--- 0x10-ES6_classes/1-make_classrooms.js
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,10 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
+
 export default function initializeRooms() {
-    const result = [
-      new ClassRoom(19),
-      new ClassRoom(20),
-      new ClassRoom(34),
-    ];
-    return result;
-  }
+  const result = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+  return result;
+}
