9c30c525c70b3ce5013de7ed9ca673ce3aa61b27
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-04-02 03:37:18 +0100

    using process stdin

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,7 +1,11 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
 });
