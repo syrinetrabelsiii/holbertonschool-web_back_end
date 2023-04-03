5a7c8dda28eac787352893ee0edfa4badfc98e3f
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-04-03 23:49:23 +0100

    reading a file

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  const cs = [];
+  const swe = [];
+  const response = [];
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (e, data) => {
+      if (e) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      data.split(/\r?\n/).forEach((l) => {
+        if (l.includes('CS')) {
+          cs.push(l.split(',')[0]);
+        } else if (l.includes('SWE')) {
+          swe.push(l.split(',')[0]);
+        }
+      });
+      console.log(`Number of students: ${cs.length + swe.length}`);
+      console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+      console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+      response.push(`Number of students: ${cs.length + swe.length}`);
+      response.push(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+      response.push(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+      resolve({
+        response,
+      });
+    });
+  });
+};
