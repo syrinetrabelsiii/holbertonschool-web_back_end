f323459706d9530d6a354f5f59f670e73319992a
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-04-04 23:09:44 +0100

    add type to arguments

---
+++ 0x13-unittests_in_js/1-calcul.js
@@ -0,0 +1,18 @@
+module.exports = function calculateNumber (type, a, b) {
+    a = Math.round(a);
+    b = Math.round(b);
+    switch (type) {
+        case 'SUM':
+            return a + b;
+            case 'SUBTRACT':
+                return a - b;
+                case 'DIVIDE':
+                    if (b ===0){
+                        return 'Error';
+                    }
+                    return a / b; 
+    }
+    return 'Error';
+    
+    }
+    
