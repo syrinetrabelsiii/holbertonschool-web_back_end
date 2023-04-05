b325910d535f702b1a2ce618e220da9d45263724
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-04-05 22:42:54 +0100

    new module

---
+++ 0x13-unittests_in_js/utils.js
@@ -0,0 +1,20 @@
+const Utils = {
+calculateNumber (type, a, b) {
+    a = Math.round(a);
+    b = Math.round(b);
+    switch (type) {
+        case 'SUM':
+            return a + b;
+            case 'SUBTRACT':
+                return a - b;
+                case 'DIVIDE':
+                    if (b ===0){
+                        return 'Error';
+                    }
+                    return a / b; 
+    }
+    return 'Error';
+    
+    }
+}
+module.exports = Utils;
