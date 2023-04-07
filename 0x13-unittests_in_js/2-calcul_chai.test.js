9a98d728a0bda6e123d0c8681abac209768cbdd5
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-04-07 07:58:15 +0100

    test file chai

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,30 +1,29 @@
-const calculateNumber = require("./1-calcul.js");
-const expect = require("chai");
+const calculateNumber = require("./2-calcul_chai.js");
+const { expect } = require('chai');
 
 describe('calculateNumber', () => {
-  it('should return rounded sum', () => {
-    assert.strictEqual(calculateNumber('SUM', 1, 2), 3);
-    assert.strictEqual(calculateNumber('SUM', 1, 0), 1);
-    assert.strictEqual(calculateNumber('SUM', 1, -1), 0);
-    assert.strictEqual(calculateNumber('SUM', -1, -2), -3);
-    assert.strictEqual(calculateNumber('SUM', 1.6, 3), 5);
-    assert.strictEqual(calculateNumber('SUM',-1.2, -3.8), -5);
+  it('returns rounded sum with SUM', () => {
+    expect(calculateNumber('SUM', 1, 2)).to.equal(3);
+    expect(calculateNumber('SUM', 1, 0)).to.equal(1);
+    expect(calculateNumber('SUM', 1, -1)).to.equal(0);
+    expect(calculateNumber('SUM', -1, -2)).to.equal(-3);
+    expect(calculateNumber('SUM', 1.6, 3)).to.equal(5);
+    expect(calculateNumber('SUM', -1.2, -3.8)).to.equal(-5);
   });
-  it('should return rounded difference', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 1, 3), -2);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1, 1), 0);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1, -1), 2);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.8), -3);
-    assert.strictEqual(calculateNumber('SUBTRACT',-1.2, -3.8), 3);
+  it('returns rounded sum with SUBTRACT', () => {
+    expect(calculateNumber('SUBTRACT',1, 3)).to.equal(-2);
+    expect(calculateNumber('SUBTRACT',1, 1)).to.equal(0);
+    expect(calculateNumber('SUBTRACT',1, -1)).to.equal(2);
+    expect(calculateNumber('SUBTRACT', 1.2, 3.8)).to.equal(-3);
+    expect(calculateNumber('SUBTRACT',-1.2, -3.8)).to.equal(3);
   });
-  it('should return rounded quotient', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.2, 5.2), 0.2);
+  it('returns rounded sum with DIVIDE', () => {
+    expect(calculateNumber('DIVIDE', 1.2, 5.2)).to.equal(0.2);
   });
-  it('should return Error string for zero division', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
+  it('returns error string when DIVIDE by 0', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
   });
-  it('should return Error string for invalid operation', () => {
-    assert.strictEqual(calculateNumber('sdf', 1, 0), 'Error');
+  it('should throw error if invalid type', () => {
+    expect(calculateNumber('sdf', 2, 3)).to.equal('Error');
   });
-
 });
