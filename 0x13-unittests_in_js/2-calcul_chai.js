0f51a859c551bbb60c2008d5d191490b9b1d31f6
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-04-07 07:57:34 +0100

    chai

--- 0x13-unittests_in_js/2-calcul_chai.js
+++ 0x13-unittests_in_js/2-calcul_chai.js
@@ -1,17 +1,25 @@
-module.exports = function calculateNumber (type, a, b) {
+function calculateNumber(type, a, b) {
+
+    if (Number.isNaN(a) || Number.isNaN(b))
+      throw TypeError;
+  
     a = Math.round(a);
     b = Math.round(b);
+  
     switch (type) {
-        case 'SUM':
-            return a + b;
-            case 'SUBTRACT':
-                return a - b;
-                case 'DIVIDE':
-                    if (b ===0){
-                        return 'Error';
-                    }
-                    return a / b; 
+      case 'SUM':
+        return a + b;
+      case 'SUBTRACT':
+        return a - b;
+      case 'DIVIDE':
+        if (b === 0) {
+          return 'Error';
+        }
+        return a / b;
+      default:
+        return 'Error';
     }
-    return 'Error';
-    
-    }
+  }
+  
+  module.exports = calculateNumber;
+  
