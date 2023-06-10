90d21a89f0a90842c0bb97a1e1025673524cd006
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-06-10 01:51:55 +0100

    web

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Module: Redis Server,  Implementing an
+expiring web cache and tracker"""
+import redis
+from typing import Union, Optional, Callable
+import requests
+
+redis = redis.Redis()
+incr = 0
+
+
+def get_page(url: str) -> str:
+    """Function: expiring web cache and tracker"""
+
+    response = requests.get(url)
+    redis.set(f"cached:{url}", incr)
+    redis.incr(f"count:{url}")
+    redis.setex(f"cached:{url}", 10, redis.get(f"cached:{url}"))
+    return response.text
+
+
