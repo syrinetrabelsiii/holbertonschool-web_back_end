0b50d3edeab143949adb28bfcea6778ac8102437
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-09-25 17:57:33 +0100

    basic auth

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -32,8 +32,8 @@ def forbidden(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
