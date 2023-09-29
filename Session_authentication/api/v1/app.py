ba744e23b95a88c987e28f41c515ba01f53162ca
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-09-29 02:13:45 +0100

    api

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -12,7 +12,35 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
+if getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+
+
+@app.before_request
+def before_request() -> None:
+    """ Before request
+    """
+    if auth is not None:
+        exclude_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                         '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+        require_auth = auth.require_auth(path=request.path,
+                                         excluded_paths=exclude_paths)
+        if require_auth:
+            if not auth.authorization_header(request) and not \
+               auth.session_cookie(request):
+                abort(401)
+            if not auth.current_user(request):
+                abort(403)
+            request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
@@ -22,20 +50,21 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
-    
 @app.errorhandler(401)
-def Unauthorized(error) -> str:
-    """ Unauthorized handler
+def unauthorized(error) -> str:
+    """ unauthorized error handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden handler
+def not_allowed(error) -> str:
+    """ not allowed error handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -25,17 +25,25 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
-if not user_id:
+    if user_id is None:
         abort(404)
-    if user_id == 'me':
-        if not request.current_user:
-            abort(404)
-        user = User.get(request.current_user.id)
-        return jsonify(user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
+    if user != request.current_user:
+        abort(404)
     return jsonify(user.to_json())
+
+
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def me() -> str:
+    """ GET /api/v1/users/me
+    Return:
+      - User object JSON represented
+      - 404 if the User ID doesn't exist
+    """
+    return jsonify(request.current_user.to_json())
+
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
